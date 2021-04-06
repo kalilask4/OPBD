ce603e968bfcf3cc6cad08a1f1a4c50f9357e87c
Diana <57933812+kalilask4@users.noreply.github.com>
2021-04-06 23:58:21 +0300

    added some tasks

---
+++ OPBD/lb8 - locks, indexes/SQLQueryLB1.sql
@@ -0,0 +1,43 @@
+--Анализ эффективности индексов на основе планов выполнения (бд univer2)
+--Для заданных запросов создать индексы, с которыми запросы будут выполнятся не хуже, каждый созданный индекс должен закрывать как можно больше запросов из списка
+
+--1)Определить на какой факультет поступило наибольшее количество человек в 2015 году
+CREATE NONCLUSTERED
+INDEX idex_indate
+ON stud(in_date DESC)
+ 
+select TOP 1 faculty.faculty_name, count(stud.id) as count_stud  from stud 
+join process on process.stud_id = stud.id and YEAR(stud.in_date) = 2015
+join hours on process.hours_id = hours.id
+join faculty on faculty.id = hours.faculty_id
+GROUP BY faculty.faculty_name
+ORDER BY count_stud desc
+
+--2)Вывести список студентов с фамилией на 'З'
+CREATE NONCLUSTERED
+INDEX idex_Last_name
+ON stud(last_name DESC)
+ 
+select stud.last_name from stud
+where last_name like 'З%'
+
+--3) Вывести список студентов, средний балл которых меньше  7
+CREATE NONCLUSTERED
+INDEX idex_exm
+ON stud(last_name DESC)
+select last_name, exm from stud 
+where exm < 7
+
+--4) Вывести список студентов, средний балл которых меньше чем средний по институту
+select last_name, exm from stud 
+where exm < (select AVG(exm) from stud)
+
+--5) Определить, кто из студентов поступил в 2014 году
+select last_name,in_date from stud
+where YEAR(stud.in_date) = 2014
+
+--6)Определить у кого из студентов, кто поступил в 2015 году, наивысшая оценка
+select top 1 last_name,exm from stud
+where YEAR(stud.in_date) = 2015
+ORDER BY exm desc
+
